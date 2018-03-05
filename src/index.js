a53ab993013c841eb49847f25b87af0779986ad4
unknown <loggitec4@ramblerr.ru>
2018-03-05 18:21:06 +0300

    solution all

--- src/index.js
+++ src/index.js
@@ -11,16 +11,16 @@ module.exports = function getcurrZeroCount(number, base) {
 
 		//Определяем количество повторений делителя d для числа number
 		for (var i=0; i<10; i++)
-		if (currentBase % d === 0) {nuberOfDevide++; currentBase=currentBase/d;}
+			if (currentBase % d === 0) {nuberOfDevide++; currentBase=currentBase/d;}
 		//	console.log(nuberOfDevide);
 		//	console.log(currentBase);
 				
         //Определяем количество нулей для делителя d 
         for (var i = d; i <= number; i=i*d) 
-		currZero += Math.trunc(number/i);
+			currZero += Math.trunc(number/i);
 		//console.log(currZero);
 		
-         MinOfZero = Math.min(MinOfZero, (currZero / nuberOfDevide))
+        MinOfZero = Math.min(MinOfZero, (currZero / nuberOfDevide))
 		console.log(MinOfZero);
     }
     return Math.trunc(MinOfZero);
